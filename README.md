971e5fa0d77a0992b951596fbe05b815892076e5
Taylor Johannsen <tjohannsen@mxssolutions.com>
2019-12-07 11:52:38 -0800

    added start scripts

--- README.md
+++ README.md
@@ -1,1 +1,2 @@
 # smite-stats
+React - Node application that stores user data from Smite (https://www.smitegame.com/) to display on a website and to be endpoints for a discord bot to hit. 
--- backend/app.js
+++ backend/app.js
@@ -2,4 +2,4 @@ const express = require('express');
 
 const app = express();
 
-app.listen(3000, () => console.log('Started Node on port 3000!'));
+app.listen(3001, () => console.log('Started Node on port 3001!'));
