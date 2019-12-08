cdf4186aa83095516016beb8ecbc3b7d35f93dce
Taylor Johannsen <tjohannsen@mxssolutions.com>
2019-12-08 09:46:35 -0800

    messing with hirez api

--- backend/app.js
+++ backend/app.js
@@ -1,5 +1,26 @@
 const express = require('express');
+const Hirez = require('hirez.js');
 
 const app = express();
 
 app.listen(3001, () => console.log('Started Node on port 3001!'));
+
+let hirez = new Hirez({
+    devId: '3420',
+    authKey: 'A26A258DC35048DEA884D8DDF99DBD56'
+})
+
+hirez.smite('pc').session.generate()
+    .then((res) => {
+        hirez.smite('pc').getGodRanks('taylortj')
+            .then(ranks => {
+                let topGods = ranks.filter(rank => {
+                    return rank.Rank > 5;
+                });
+
+                console.log(topGods)
+            })
+    })
+    .catch(err => {
+        console.log(err);
+    })
