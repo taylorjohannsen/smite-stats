910141859587815bb874791457b777248deecfd0
Taylor Johannsen <tjohannsen@mxssolutions.com>
2019-12-08 09:58:48 -0800

    created key file

--- backend/app.js
+++ backend/app.js
@@ -1,24 +1,23 @@
 const express = require('express');
 const Hirez = require('hirez.js');
+const creds = require('./utility/dev-key');
 
 const app = express();
 
 app.listen(3001, () => console.log('Started Node on port 3001!'));
 
 let hirez = new Hirez({
-    devId: '3420',
-    authKey: 'A26A258DC35048DEA884D8DDF99DBD56'
+    devId: creds.devid,
+    authKey: creds.authKey
 })
 
 hirez.smite('pc').session.generate()
     .then((res) => {
-        hirez.smite('pc').getGodRanks('taylortj')
-            .then(ranks => {
-                let topGods = ranks.filter(rank => {
-                    return rank.Rank > 5;
-                });
+        hirez.smite('pc').getMatchHistory('taylortj')
+            .then(player => {
+                
 
-                console.log(topGods)
+                console.log(player)
             })
     })
     .catch(err => {
